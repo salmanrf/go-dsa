4da499d9b4c5420ef8fe38401113762189093e5e
m <salmanrf2@gmail.com>
2025-05-15 08:50:56 +0800

    feat: binary search trees

--- 4-trees/binary_trees/binary_search_tree.go
+++ 4-trees/binary_trees/binary_search_tree.go
@@ -1,6 +1,6 @@
 package binary_trees
 
-type TraversalCompare[T any] func (new T, data *Node[T]) int
+type TraversalCompare[T any] func (new T, node *Node[T]) int
 
 func BSTFromArray[T any](items []T, compare TraversalCompare[T]) *Node[T] {
 	size := len(items);
@@ -37,45 +37,35 @@ func BSTSearch[T any](root *Node[T], target T, compare TraversalCompare[T]) *Nod
 func BSTInsert[T any](root *Node[T], data T, compare TraversalCompare[T]) *Node[T] {
 	// * 1. If root is nil, create a new node (root)
 	// * 2. If data is less than root.data, insert to left,
-	// * 3. If data is greater than root.data, insert to right,	
+	// * 3. If data is greater than root.data, insert to right,
+	// Can we omit returning root at the end ? 
+	// This should have worked because we're working with pointers. If we find the correct node to insert into, 
+	// it shouldn't matter because the original pointers (root.left or root.right) aren't updated directly. 
+	// Essentially, we are performing a search for the correct node, and at the end, we create a new node and return it.
+	// Ahhh, I get it now! We need to create a new node and return it at some point, and this return has to be accepted 
+	// somewhere, either as root.left or root.right. Once we finish at the current frame, we still need to move back up.
+	// If we don't return the root (current node), the calling frame will receive nil, which would destroy the hierarchy.
+
+	
 	if root == nil {
 		return &Node[T]{
 			Data: data,
-		};
+		}
 	}
+
+	if(compare(data, root)) == 0 {
+		return root
+	} 
 
 	if compare(data, root) <= 0 {
-		root.Left = BSTInsert(root.Left, data, compare);
+		root.Left = BSTInsert(root.Left, data, compare)
+	} else {
+		root.Right = BSTInsert(root.Right, data, compare)
 	}
 
-	if compare(data, root) > 0 {
-		root.Right = BSTInsert(root.Right, data, compare);
-	}
-
-	return root;
+	return root
 }
 
-func GetInorderSuccessor[T any](root *Node[T], value T, parentCompare TraversalCompare[T], compare TraversalCompare[T]) *Node[T] {
-	node := BSTSearch(
-		root,
-		value,
-		compare,
-	)
 
-	if node == nil {
-		return nil
-	}
-
-	if node.Right == nil {
-		
-	}
-	
-	// ? Get parent of target node, in case it doesn't have a right sub-tree
-	parent := BSTSearch(
-		root,
-		value,
-		parentCompare,
-	)
-}
 
 
---
+++ 4-trees/binary_trees/bst_test.go
@@ -0,0 +1,152 @@
+package binary_trees
+
+import "testing"
+
+func TestNew(t *testing.T) {
+	data := 10
+
+	want := data
+
+	got := New(data, func(a, b int) int {
+		return a - b
+	}).Data
+
+	if got != want {
+		t.Errorf("Got %d, want %d", got, want)
+	}
+}
+
+func TestInsert(t *testing.T) {
+	root := New(10, func(a, b int) int {
+		return a - b
+	})
+
+	root.Insert(5)
+	root.Insert(15)
+	root.Insert(6)
+	root.Insert(12)
+
+	want := 5
+
+	got := root.Left.Data
+
+	if got != want {
+		t.Errorf("Got %d, want %d", got, want)
+	} 
+
+	want = 6
+
+	got = root.Left.Right.Data
+
+	if got != want {
+		t.Errorf("Got %d, want %d", got, want)
+	}
+}
+
+func TestMin(t *testing.T) {
+	root := New(10, func(a, b int) int {
+		return a - b
+	})
+
+	root.Insert(5)
+	root.Insert(15)
+	root.Insert(6)
+	root.Insert(12)
+
+	want := 5
+
+	got := root.Min()
+
+	if got.Data != want {
+		t.Errorf("Got %d, want %d", got.Data, want)
+	}
+}
+
+func TestMax(t *testing.T) {
+	tests := []struct{
+		name string
+		elements []int
+		want int
+	} {
+		{
+			"Case 1",
+			[]int{10, 5, 15, 6, 12},
+			15,
+		},
+		{
+			"Case 2",
+			[]int{100, 50, 25, 12, 6, 3},
+			100,
+		},
+		{
+			"Case 3",
+			[]int{0, 1, 2, 3, 4, 5},
+			5,
+		},
+	}
+	
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			root := New(tt.elements[0], func(a, b int) int {
+				return a - b
+			})
+
+			for _, el := range tt.elements[1:] {
+				root.Insert(el)
+			}
+			
+			want := tt.want
+		
+			got := root.Max()
+		
+			if got.Data != want {
+				t.Errorf("Got %d, want %d", got.Data, want)
+			}
+		})
+	}
+}
+
+func TestDelete(t *testing.T) {
+	tests := []struct{
+		name 				string
+		elements  	[]int
+		del 				int
+		replacement int
+	}{
+		{
+			"Delete Node with right subtree",
+			[]int{1, 2, 3, 4, 5},
+			3,
+			4,
+		},
+		{
+			"Delete Node with left & right subtree",
+			[]int{20, 10, 30},
+			20,
+			30,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func (t *testing.T) {
+			root := New(tt.elements[0], func(a, b int) int {
+				return a - b
+			})
+
+			for _, el := range tt.elements[1:] {
+				root.Insert(el)
+			}
+			
+			root.Delete(tt.del)
+
+			t.Run("The deleted node should not exist anymore", func (t *testing.T) {
+				var want *BSTNode[int] = nil 
+				got := root.Search(tt.del)
+			
+				if got != want {
+					t.Errorf("Got %p, want %p", got, want)
+				}
+			})
+		})
+	}
+}
